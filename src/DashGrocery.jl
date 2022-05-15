aa1bcb53dbd17ea7ae24b807b2b6c368a9a071d2
Gyasi <Gyasi.i@outlook.com>
2022-05-15 07:22:09 -0700

    .

--- src/DashGrocery.jl
+++ src/DashGrocery.jl
@@ -3,7 +3,7 @@ module DashGrocery
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.1"
+const version = "0.0.2"
 
 include("jl/barcode.jl")
 include("jl/clock.jl")

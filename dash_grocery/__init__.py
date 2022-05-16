dc5072e79f044856e98e072d889f514bf8d92f2b
Gyasi <Gyasi.i@outlook.com>
2022-05-16 10:49:12 -0700

    .

--- dash_grocery/__init__.py
+++ dash_grocery/__init__.py
@@ -10,19 +10,22 @@ import dash as _dash
 from ._imports_ import *
 from ._imports_ import __all__
 
-if not hasattr(_dash, '__plotly_dash') and not hasattr(_dash, 'development'):
-    print('Dash was not successfully imported. '
-          'Make sure you don\'t have a file '
-          'named \n"dash.py" in your current directory.', file=_sys.stderr)
+if not hasattr(_dash, "__plotly_dash") and not hasattr(_dash, "development"):
+    print(
+        "Dash was not successfully imported. "
+        "Make sure you don't have a file "
+        'named \n"dash.py" in your current directory.',
+        file=_sys.stderr,
+    )
     _sys.exit(1)
 
 _basepath = _os.path.dirname(__file__)
-_filepath = _os.path.abspath(_os.path.join(_basepath, 'package-info.json'))
+_filepath = _os.path.abspath(_os.path.join(_basepath, "package-info.json"))
 with open(_filepath) as f:
     package = json.load(f)
 
-package_name = package['name'].replace(' ', '_').replace('-', '_')
-__version__ = package['version']
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+__version__ = package["version"]
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
@@ -36,10 +39,9 @@ _js_dist.extend(
     [
         {
             "relative_package_path": "async-{}.js".format(async_resource),
-            "external_url": (
-                "https://unpkg.com/{0}@{2}"
-                "/{1}/async-{3}.js"
-            ).format(package_name, __name__, __version__, async_resource),
+            "external_url": ("https://unpkg.com/{0}@{2}" "/{1}/async-{3}.js").format(
+                package_name, __name__, __version__, async_resource
+            ),
             "namespace": package_name,
             "async": True,
         }
@@ -53,8 +55,7 @@ _js_dist.extend(
         {
             "relative_package_path": "async-{}.js.map".format(async_resource),
             "external_url": (
-                "https://unpkg.com/{0}@{2}"
-                "/{1}/async-{3}.js.map"
+                "https://unpkg.com/{0}@{2}" "/{1}/async-{3}.js.map"
             ).format(package_name, __name__, __version__, async_resource),
             "namespace": package_name,
             "dynamic": True,
@@ -65,17 +66,12 @@ _js_dist.extend(
 
 _js_dist.extend(
     [
+        {"relative_package_path": "dash_grocery.min.js", "namespace": package_name},
         {
-            'relative_package_path': 'dash_grocery.min.js',
-    
-            'namespace': package_name
+            "relative_package_path": "dash_grocery.min.js.map",
+            "namespace": package_name,
+            "dynamic": True,
         },
-        {
-            'relative_package_path': 'dash_grocery.min.js.map',
-    
-            'namespace': package_name,
-            'dynamic': True
-        }
     ]
 )
 
@@ -83,5 +79,5 @@ _css_dist = []
 
 
 for _component in __all__:
-    setattr(locals()[_component], '_js_dist', _js_dist)
-    setattr(locals()[_component], '_css_dist', _css_dist)
+    setattr(locals()[_component], "_js_dist", _js_dist)
+    setattr(locals()[_component], "_css_dist", _css_dist)

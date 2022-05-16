62420ddb8563de5e61e39813ede2d200127803e4
Gyasi <Gyasi.i@outlook.com>
2022-05-16 09:03:13 -0700

    up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -94,7 +94,7 @@ module.exports = (env, argv) => {
                 })
             ],
             splitChunks: {
-                name: true,
+                name: false,
                 cacheGroups: {
                     async: {
                         chunks: 'async',

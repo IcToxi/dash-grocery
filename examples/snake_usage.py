15d81254b50be0fc301d15982276c9dd4e69ad42
IcToxi <ts.ii@outlook.com>
2022-05-19 11:03:36 -0700

    .

--- examples/snake_usage.py
+++ examples/snake_usage.py
@@ -12,4 +12,4 @@ app.layout = html.Div(
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
--- examples/weather_usage.py
+++ examples/weather_usage.py
@@ -40,4 +40,4 @@ def update(n, lat, lon, city):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)

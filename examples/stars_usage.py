ce8f6cb503afcf46efee8c613e4f867c1d683bc0
IcToxi <ts.ii@outlook.com>
2022-06-30 22:47:53 -0700

    .

--- examples/stars_usage.py
+++ examples/stars_usage.py
@@ -6,7 +6,13 @@ from dash import Dash, html
 
 app = Dash(__name__)
 
-app.layout = html.Div(dash_grocery.Stars(count=5, size=150, edit=True))
+app.layout = html.Div(
+    [
+        dash_grocery.Stars(count=5, color2="red", size=150, char="❤", edit=True),
+        dash_grocery.Stars(count=5, value=3, size=150, char="☻", edit=False),
+        dash_grocery.Stars(count=5, size=150, edit=True),
+    ]
+)
 
 
 if __name__ == "__main__":
